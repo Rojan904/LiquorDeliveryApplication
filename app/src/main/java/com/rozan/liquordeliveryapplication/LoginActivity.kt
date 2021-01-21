6eae96a52cca6076890f881a8ecb316f57e12372
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-01-21 20:19:29 +0545

    Added back button with icons.

--- app/src/main/java/com/rozan/liquordeliveryapplication/LoginActivity.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.rozan.liquordeliveryapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var tvSignUp:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tvSignUp=findViewById(R.id.tvSignUp)
+        tvSignUp.setOnClickListener{
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
     }
 }
