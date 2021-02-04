f1b4f5a65a280c399870595f826e2c38ed17b45f
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-02-05 00:30:49 +0545

    Checked logged in user in splash screen.

--- app/src/main/java/com/rozan/liquordeliveryapplication/SplashScreenActivity.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/SplashScreenActivity.kt
@@ -16,8 +16,22 @@ class SplashScreenActivity : AppCompatActivity() {
         //Background thread, usually for showing while server checks user details.
         CoroutineScope(Dispatchers.IO).launch { //coroutine scope is a light weight thread.
             delay(1000)
-            startActivity(Intent(this@SplashScreenActivity,LoginActivity::class.java))
+
+
+            checkLogin()
             finish()
+        }
+    }
+
+    private fun checkLogin() {
+    val sharedPref=getSharedPreferences("MyPref", MODE_PRIVATE)
+    val username=sharedPref.getString("username","")
+    val password=sharedPref.getString("password","")
+        if (username != null && !username.equals("")) {
+            startActivity(Intent(this@SplashScreenActivity,AilaActivity::class.java))
+    }
+        else{
+            startActivity(Intent(this@SplashScreenActivity,LoginActivity::class.java))
         }
     }
 }
