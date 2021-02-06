7957352fe4a11de6b33de90df3e1f8809d7cb1c3
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-02-06 22:47:40 +0545

    Added progreesbar in splashscreen.

--- app/src/main/java/com/rozan/liquordeliveryapplication/SplashScreenActivity.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/SplashScreenActivity.kt
@@ -3,21 +3,33 @@ package com.rozan.liquordeliveryapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.DoubleBounce
+import com.github.ybq.android.spinkit.style.Wave
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+
         //Background thread, usually for showing while server checks user details.
         CoroutineScope(Dispatchers.IO).launch { //coroutine scope is a light weight thread.
-            delay(1000)
+            delay(3000)
+            runOnUiThread(kotlinx.coroutines.Runnable {
 
+                progressBar = findViewById(R.id.spin_kit)
+                val doubleBounce: Sprite = DoubleBounce()
+                progressBar.indeterminateDrawable = doubleBounce
+//                finish()
 
+            })
             checkLogin()
             finish()
         }
@@ -28,9 +40,11 @@ class SplashScreenActivity : AppCompatActivity() {
     val username=sharedPref.getString("username","")
     val password=sharedPref.getString("password","")
         if (username != null && !username.equals("")) {
+
             startActivity(Intent(this@SplashScreenActivity,AilaActivity::class.java))
     }
         else{
+
             startActivity(Intent(this@SplashScreenActivity,LoginActivity::class.java))
         }
     }
