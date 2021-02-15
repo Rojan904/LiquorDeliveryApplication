fdc55750e5c3090c3c17380ff25d41abc410678c
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-02-16 00:06:06 +0545

    Modified entity User

--- app/src/main/java/com/rozan/liquordeliveryapplication/entity/User.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/entity/User.kt
@@ -1,9 +1,5 @@
 package com.rozan.liquordeliveryapplication.entity
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
 data class User(
         var firstName:String?=null,
         var lastName:String?=null,
@@ -12,8 +8,4 @@ data class User(
         var email:String?=null,
         var password:String?=null
 
-)  {
-    @PrimaryKey(autoGenerate = true)
-    var userId:Int=0
-
-}
+)
