0661742461010b032dff399c7f60fdeaf17a8493
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-02-16 18:44:11 +0545

    Added user repository with api request.

---
+++ app/src/main/java/com/rozan/liquordeliveryapplication/repository/UserRepository.kt
@@ -0,0 +1,16 @@
+package com.rozan.liquordeliveryapplication.repository
+
+import com.rozan.liquordeliveryapplication.api.APIRequest
+import com.rozan.liquordeliveryapplication.api.ServiceBuilder
+import com.rozan.liquordeliveryapplication.api.UserAPI
+import com.rozan.liquordeliveryapplication.entity.User
+import com.rozan.liquordeliveryapplication.response.RegistrationResponse
+
+class UserRepository:APIRequest() {
+    val api=ServiceBuilder.buildService(UserAPI::class.java)
+    suspend fun registerUser(user:User):RegistrationResponse{
+    return apiRequest {
+        api.registerUser(user)
+    }
+}
+}
