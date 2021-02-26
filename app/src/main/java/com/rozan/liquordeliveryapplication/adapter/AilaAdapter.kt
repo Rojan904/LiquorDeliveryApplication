26daf052f3d03e06559baeb093401e9da43e5fbd
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-02-26 23:45:35 +0545

    Recyclerview added.

---
+++ app/src/main/java/com/rozan/liquordeliveryapplication/adapter/AilaAdapter.kt
@@ -0,0 +1,54 @@
+package com.rozan.liquordeliveryapplication.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.rozan.liquordeliveryapplication.R
+import com.rozan.liquordeliveryapplication.entity.Aila
+
+class AilaAdapter (
+        val lstAila:List<Aila>,
+        val context: Context
+        ):RecyclerView.Adapter<AilaAdapter.AilaViewHolder>()
+{
+    class AilaViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val ailaImage:ImageView
+        val ailaPrice:TextView
+        val ailaMl:TextView
+        val ailaName:TextView
+        val imgFavorite:ImageView
+        val imgCart:ImageView
+
+        init {
+            ailaImage=view.findViewById(R.id.image)
+            ailaPrice=view.findViewById(R.id.price)
+            ailaMl=view.findViewById(R.id.ml)
+            ailaName=view.findViewById(R.id.name)
+            imgFavorite=view.findViewById(R.id.imgFavorite)
+            imgCart=view.findViewById(R.id.imgCart)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AilaViewHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.show_aila_layout,parent,false)
+        return AilaViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AilaViewHolder, position: Int) {
+        val aila=lstAila[position]
+        holder.ailaPrice.text=aila.ailaPrice.toString()
+        holder.ailaMl.text=aila.ailaMl.toString()
+        holder.ailaName.text=aila.ailaName.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return lstAila.size
+    }
+}
---
+++ app/src/main/java/com/rozan/liquordeliveryapplication/entity/Aila.kt
@@ -0,0 +1,16 @@
+package com.rozan.liquordeliveryapplication.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+data class Aila (
+    var ailaImage:String?=null,
+    var ailaPrice:Double?=null,
+    var ailaMl:String?=null,
+    var ailaName:String?=null,
+    var ailaType:String?=null,
+
+        ){
+
+}
