4bf65d8ce39402348bce1e2ed7224dcdce927324
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-02-28 00:00:08 +0545

    Recyclerview for category.

---
+++ app/src/main/java/com/rozan/liquordeliveryapplication/model/AilaCategory.kt
@@ -0,0 +1,7 @@
+package com.rozan.liquordeliveryapplication.model
+
+data class AilaCategory(
+    val categId:Int?=null,
+    val categName:String?=null,
+    val categImage:String?=null
+)
--- app/src/main/java/com/rozan/liquordeliveryapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/ui/home/HomeFragment.kt
@@ -12,12 +12,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rozan.liquordeliveryapplication.R
 import com.rozan.liquordeliveryapplication.adapter.AilaAdapter
+import com.rozan.liquordeliveryapplication.adapter.AilaCategoryAdapter
 import com.rozan.liquordeliveryapplication.entity.Aila
+import com.rozan.liquordeliveryapplication.model.AilaCategory
 
 class HomeFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var categRecyclerView: RecyclerView
 
     private var ailaList= arrayListOf<Aila>()
+    private var ailaCategory= arrayListOf<AilaCategory>()
     private lateinit var homeViewModel: HomeViewModel
     override fun onAttach(context: Context) {
         super.onAttach(requireContext())
@@ -34,11 +38,25 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             val context=root.context
+            categoryRecyclerView(root,context)
             ailaRecyclerView(root,context)
         })
 
 
         return root
+    }
+
+    private fun categoryRecyclerView(view: View, context: Context) {
+        categRecyclerView=view.findViewById(R.id.categRecyclerView)
+        val adapter=AilaCategoryAdapter(ailaCategory,context)
+        categRecyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        categRecyclerView.adapter = adapter
+
+        loadCateg()
+    }
+
+    private fun loadCateg() {
+        ailaCategory.add((AilaCategory(categName = "Whisky",categImage = "https://cdn.shopify.com/s/files/1/0025/0399/9545/products/40020_1.jpg?v=1575660313")))
     }
 
     private fun ailaRecyclerView(view: View,context: Context) {
