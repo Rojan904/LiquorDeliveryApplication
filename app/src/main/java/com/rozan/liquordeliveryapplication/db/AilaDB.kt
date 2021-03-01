e6b89e7e0e79f571bc3f58c2a1aa8cb90ced4479
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-03-01 13:56:02 +0545

    Data processed with room.

--- app/src/main/java/com/rozan/liquordeliveryapplication/db/AilaDB.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/db/AilaDB.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.rozan.liquordeliveryapplication.dao.AilaDAO
 import com.rozan.liquordeliveryapplication.dao.UserDAO
+import com.rozan.liquordeliveryapplication.entity.Aila
 import com.rozan.liquordeliveryapplication.entity.User
 
 @Database(
-        entities =[User::class],
+        entities =[(User::class),(Aila::class)],
         version = 1,
         exportSchema = false
 )
 abstract class AilaDB:RoomDatabase() {
     abstract fun getUserDAO():UserDAO  //instance of UserDAO interface to access the function
+    abstract fun getAilaDAO():AilaDAO
     companion object{
         @Volatile
         private var instance:AilaDB?=null
