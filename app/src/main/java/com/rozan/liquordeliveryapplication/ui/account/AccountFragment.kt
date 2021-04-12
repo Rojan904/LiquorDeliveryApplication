e01b9e9a2d8129d0a6a9ee75fb77ba04b2909c2c
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2021-04-12 22:11:35 +0545

    Added profile page design.

--- app/src/main/java/com/rozan/liquordeliveryapplication/ui/account/AccountFragment.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/ui/account/AccountFragment.kt
@@ -6,26 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
 import com.rozan.liquordeliveryapplication.R
+import com.rozan.liquordeliveryapplication.ui.order.OrderViewModel
 
 class AccountFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AccountFragment()
-    }
+    private lateinit var accountViewModel: AccountViewModel
 
-    private lateinit var viewModel: AccountViewModel
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        accountViewModel =
+                ViewModelProvider(this).get(AccountViewModel::class.java)
+        val root = inflater.inflate(R.layout.account_fragment, container, false)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.account_fragment, container, false)
-    }
+        accountViewModel.text.observe(viewLifecycleOwner, Observer {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
-        // TODO: Use the ViewModel
+        })
+        return root
     }
 
 }
--- app/src/main/java/com/rozan/liquordeliveryapplication/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/ui/account/AccountViewModel.kt
@@ -1,7 +1,12 @@
 package com.rozan.liquordeliveryapplication.ui.account
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
 }
