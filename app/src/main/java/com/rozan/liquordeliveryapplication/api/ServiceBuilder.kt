6244c71997dc0a374fa622e63abb8b529a3be816
Rozan Shrestha <rozan.cresta28@gmail.com>
2021-09-06 10:54:14 +0545

    Liquor shop

--- app/src/main/java/com/rozan/liquordeliveryapplication/api/ServiceBuilder.kt
+++ app/src/main/java/com/rozan/liquordeliveryapplication/api/ServiceBuilder.kt
@@ -6,10 +6,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private const val BASE_URL =
-            "http://10.0.2.2:90/"
 //    private const val BASE_URL =
-//        "http://192.168.137.134:90/"
+//            "http://10.0.2.2:90/"
+    private const val BASE_URL =
+        "http://192.168.137.134:90/"
     var token: String? = null
     var userId:String?=null
     var id:String?=null
